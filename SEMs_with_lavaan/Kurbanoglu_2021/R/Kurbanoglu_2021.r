fd0fbecf431a0ca23f1fb896380df183c50b6a1e
smusp <smuspratt54@gmail.com>
2025-08-24 16:43:25 +1000

    Add Kurbanoglu

---
+++ SEMs_with_lavaan/Kurbanoglu_2021/R/Kurbanoglu_2021.r
@@ -0,0 +1,56 @@
+## Kurbanoglu, N. & Takunyaci, M. (2021). A structural equation modeling
+## on relationship between self-efficacy, physics laboratory anxiety
+## and attitudes. Journal of Family, Counseling and Education, 6(1), 47-56.
+
+## Load packages
+library(lavaan)
+library(semmcci)  # For Monte Carlo CIs
+
+## Get the data from Table 1
+cor <- c(
+   1,
+   0.30,  1,
+  -0.42, -0.32,  1)
+
+sds <- c(8.81, 7.95, 18.30)
+means <- c(56.57, 40.39, 68.22)
+n <- 513
+
+## Get the variable names
+names <- c("Att", "SE", "Anx")
+
+## Get the co/variance matrix
+cov <- lavaan::getCov(cor, sds = sds, names = names)
+
+## The model
+model <- "
+  # direct effect
+  Anx ~ cpr * SE
+
+  # effects via the mediator
+  Att ~ a * SE
+  Anx ~ b * Att
+
+  # indirect effect (a * b)
+  ab := a * b
+
+  # total effect
+  total := cpr + (a * b)
+"
+
+## Fit the model and get the summary
+#  Compare with Figure 1
+fit <- sem(model, sample.cov = cov, sample.nobs = n)
+summary(fit, rsquare = TRUE, standardized = TRUE, fit.measures = TRUE)
+
+## To get intercepts
+fit_intercepts <- sem(model, sample.cov = cov, sample.nobs = n,
+   sample.mean = means)
+summary(fit_intercepts, rsquare = TRUE, standardized = TRUE)
+
+## To get Monte Carlo CIs
+semmcci::MC(fit, R = 50000, alpha = 0.05)
+
+
+
+
