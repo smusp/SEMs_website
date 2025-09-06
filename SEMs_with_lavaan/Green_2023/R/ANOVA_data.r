3b7e3f4c022a345c4ebba2fc7895a35e9028a529
smusp <smuspratt54@gmail.com>
2025-09-06 17:50:40 +1000

    Minor change to Green data

--- SEMs_with_lavaan/Green_2023/R/ANOVA_data.r
+++ SEMs_with_lavaan/Green_2023/R/ANOVA_data.r
@@ -8,26 +8,13 @@ df$id <- c(rep(1:tab[1], 2), rep(1:tab[2], 2), rep(1:tab[3], 2))  # id variable
 df <- reshape(df, timevar = "c", idvar = c("id", "x", "g"), varying = c("pre", "y"), 
    direction = "wide")
 
-
 df <- within(df, {
 ## Grand mean centered "pre" - the before scores
    preC <- scale(pre, scale = FALSE)
-
-## Dummy variables for "Coping Startegy"
-   x1 <- ifelse(x == "a", 1, 0)
-   x2 <- ifelse(x == "b", 1, 0)
-   x3 <- ifelse(x == "c", 1, 0)
 
 ## Drop the id variable
    id <- NULL
 
 ## Gender X Coping Strategy interaction
   sg <- interaction(x, g, sep = "")
-
-## Dummy variables for interaction
-  dummies <- model.matrix(~ sg - 1)
 })
-
-## Unnest the nested 'dummies' matrix, and rename its colomns
-df <- do.call(data.frame, df)
-names(df) <- gsub("dummies.sg", "", names(df))
